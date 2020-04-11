c49e20385ea11728fad8e8803375ddac43865ab9
Alex Crocker <hello@crock.dev>
2020-04-11 16:00:57 -0400

    Removed accidentally pushed scrap file

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-possibleCharacters = []
-possibleCharacters[:0] = 'abcdefghijklmnopqrstuvwxyz0123456789'
-print(possibleCharacters)
-finalString = ""
-
-for character in possibleCharacters:
-    finalString = 
